69dfff69d4b1552f1e992314a476fb142697f60d
Richard Kazanowski <rjkaz@comcast.net>
2020-08-03 15:17:27 -0400

    update

--- script.js
+++ script.js
@@ -130,4 +130,114 @@ console.log(!angry);
 console.log(5 === 5);
 console.log(5 === 10);
 
-// console loging 5 === 5 results in true. When comparing there are only two results, true or false. 
+// console loging 5 === 5 results in true. When comparing there are only two results, true or false. But as with all things, event he Equality operator has its opposite, the inequality operator!
+
+//  instead of three equals in a row, the Ineqaulity operator is an Exclamation Point with two equals in a row =  "  !==  " this checks if a number isn't equal to another
+
+console.log(5 !== 5);
+console.log(5 !== 10);
+
+// And you can store the result of a comparsion in a variable
+
+var result1 = 5 !== 5;
+var result2 = 5 === 5;
+console.log(result1);
+console.log(result2);
+
+// Now lets move on to the less-then operator "  <  " which checks if a number is less then another number
+var result3 = 1 < 90;
+console.log(result3);
+
+// that returns true in the console because 1 is less then 90, it the number wasn't less then 90, it would return false
+
+var result4 = 100 < 90;
+console.log(result4);
+
+// same rules apply for the greater then operator "  > " which checks if a number is greater then another number
+
+console.log(101 > 90);
+console.log(70 > 90);
+
+var result5 = 1000 > 90;
+console.log(result5);
+
+// We can also compare strings using the equality and ineqaulity oprerators 
+
+console.log("apple" === "apple");
+console.log("apple" !== "apple");
+
+// and of course this also applies to strings held in variables
+
+var myProblem = "I can't find a job...";
+var solution = "..so I will conquor the world instead!";
+console.log(myProblem === solution);
+console.log(myProblem !== solution);
+
+// Now just to reiterate, values can come in many types, but the big three are Strings, numbers, and booleans. 
+// strings are words or numbers wrapped in quotes, numbers are just numbers without quotes, and the boolean value is True or False with no quotes
+
+// Now lets have some fun with Math (I always hated math). By using subtraction "  -  ", addition "  +  ", multiplication "  *  ", and Division "  /  " we can do basic math in a console log or use varibales. This also applies to strings since they can be added together 
+
+console.log(8 + 8);
+console.log(90 - 3);
+console.log(8 * 9);
+console.log(100 / 5);
+
+//numbers you can use any math operator, but for strings you can only use the addition "  +  ", attempting to use -,*,/ with two strings will return NaN (not a number)
+console.log("lets go " + "to the shore!");
+console.log("lets go " - "to the shore!");
+console.log("lets go " * "to the shore!");
+console.log("lets go " / "to the shore!");
+
+//it works with variables too
+
+var numbery = 10;
+var nymberx = 9;
+console.log(numbery + nymberx);
+
+// Now if you add/join a string with a number, the number automatically gets turned into a string
+var resultR = "10" + 10;
+console.log(resultR);
+// that results in the return of the string "1010"
+
+var score = 99;
+var resultT = "Your score: " + score;
+console.log(resultT);
+
+//just want to test out what happens when you combine strings and numbers, but with the other math operators
+
+console.log("score " + 10);
+console.log("score " - 10);
+console.log("score " * 10);
+console.log("score " / 10);
+
+// same, only additon works, mixing String with numbers with anything but addition results in NaN
+
+// You can also join strings and booleans
+console.log("You passed: " + true);
+console.log("you passed: " + false);
+
+// now what happends when you try joining a number and a boolean?
+console.log(90 + true);
+// interesting...so when you combine a number value with true, the result is still a number, but adding it with true increases the number by 1. this results in 91
+console.log(90 - true);
+// subtractng the number with true decreases it by a number, this results in 89
+console.log(90 * true);
+console.log(90 / true);
+
+// multiplying and divding a number by true just returns the original number unchanged. In this case 90.
+
+// now what about the false value
+console.log(90 + false);
+console.log(90 - false);
+//so adding or subtracting a number wit the boolean false results in the original number unchanged, it stays 90
+console.log(90 * false);
+// multiplying a number with a boolean false is the same as multiplying number by 0, in this case result is 0
+console.log(900 / false);
+// dividing a number by the boolean false has the most unique effect, the results in Infinity. But despite it being a word, it still comes back as a number or boolean value, it doesn't come back as a string
+
+var word1 = 9 / false;
+var word2 = " War";
+console.log (word1 + word2);
+var word3 = "Avengers ";
+console.log (word3 + word1 + word2);
