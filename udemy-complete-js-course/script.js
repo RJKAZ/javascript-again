535b348bb44896d6f54667ff81bafa442a5de3dd
Richard Kazanowski <rjkaz@comcast.net>
2021-01-24 23:53:22 -0500

    update

---
+++ udemy-complete-js-course/script.js
@@ -0,0 +1,11 @@
+// JavaScript Fundamentals - Part 1
+
+// Values and Variables - the assignment called for using only the let method, but I used let along with var and const to demonstrate all three. 
+
+let country = "USA";
+var continent = "North America";
+const population = 328000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
