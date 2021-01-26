4c02b0e982b4a9fe93d7300634fbab57949273c2
Richard Kazanowski <rjkaz@comcast.net>
2021-01-25 23:39:57 -0500

    update

--- udemy-complete-js-course/script.js
+++ udemy-complete-js-course/script.js
@@ -1,11 +1,22 @@
 // JavaScript Fundamentals - Part 1
 
-// Values and Variables - the assignment called for using only the let method, but I used let along with var and const to demonstrate all three. 
+//  Lecture 01: Values and Variables 
+// 1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
 
 let country = "USA";
-var continent = "North America";
-const population = 328000000;
+let continent = "North America";
+let population = 328000000;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+//  LECTURE 02: Data Types
+//  1. Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
+//  2. Log the types of 'isIsland', 'population', 'country' and 'language' to the console
+
+var isIsland = false;
+var language = "";
+console.log(isIsland);
