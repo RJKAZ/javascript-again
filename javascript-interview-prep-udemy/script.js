3b0ca53b446feecced6be39dcf6cbcf7806cbe41
Reese RJ Kaz <rjkaz@comcast.net>
2021-03-16 19:59:33 -0400

    update

--- javascript-interview-prep-udemy/script.js
+++ javascript-interview-prep-udemy/script.js
@@ -6,9 +6,9 @@
 
 //tripleAdd(10)(20)(30);
 
-function trippleAdd(num1){
-    return function(num2){
-        return function(num3){
+function trippleAdd(num1) {
+    return function (num2) {
+        return function (num3) {
             console.log(num1 + num2 + num3);
         };
     };
@@ -25,16 +25,16 @@ trippleAdd(10)(20)(30);
 
 // an example of this function without couriying is
 
-function trippleAdd1 (num1, num2, num3){
+function trippleAdd1(num1, num2, num3) {
     return num1 + num2 + num3;
 }
 trippleAdd1(10, 20, 30);
 
 // another example of a couried function 
 
-function quadrupleAdd(num1){
-    return function(num2){
-        return function (num3, num4){
+function quadrupleAdd(num1) {
+    return function (num2) {
+        return function (num3, num4) {
             console.log(num1 + num2 + num3 + num4);
         }
     }
@@ -53,7 +53,7 @@ quadrupleAdd(10)(20)(30, 40);
 
 // as an example, lets take this function 
 
-function doubleNumber(num){
+function doubleNumber(num) {
     console.log(num * 2);
 }
 
@@ -72,24 +72,24 @@ doubleNumber(5);
 
 // you can even wrap your entire Javascript file within an IIFE
 
-(function() {
+(function () {
     // functions
-    function getTotal(a,b) {
+    function getTotal(a, b) {
         return a + b;
     }
     // variabales
     var $ = 'currency';
     //conditions 
-    if(true) console.log('hello world');
+    if (true) console.log('hello world');
     // and comments. You can store everything inside an IIFE
 })();
 
-//____________________________________________________________________
+//_______________________________________________________________________________________
 
 // 3rd JavaScript Question 
 
 // this function creates 5 buttons, but what happens when the 5th button is pressed
-
+/*
 function createButtons() {
     for (var i = 1; i <= 5; i++) {
       var body = document.getElementsByTagName("BODY")[0];
@@ -101,12 +101,166 @@ function createButtons() {
       body.appendChild(button);
     }
  }
-  
- createButtons();
+ 
 
- // the same thing that happenes when any of the buttons are pressed. In the initial loop, the condition is set to 5
- // so any of the buttons when clicked will return that max condition (5) + 1. So all the buttons say this is button 6
- // If I make that condition of 5 into a larger number like 10, it will create five more buttons, and when clicked, it will 
- // and alert for all 10 buttons will be 'this button is 11' since its now 10 + 1
+createButtons();
+*/
 
- // now fix this code so that it displays the corrent number when pressed. 
+// the same thing that happenes when any of the buttons are pressed. In the initial loop, the condition is set to 5
+// so any of the buttons when clicked will return that max condition (5) + 1. So all the buttons say this is button 6
+// If I make that condition of 5 into a larger number like 10, it will create five more buttons, and when clicked, it will 
+// and alert for all 10 buttons will be 'this button is 11' since its now 10 + 1
+
+// now fix this code so that it displays the corrent number when pressed. 
+
+// To do this the best way is to use an IIFE (from the last exercise)
+// the IIFE will take in a 'num' as a parameter, and we are going to immediantly invoke our IIFE with the variable I
+// then we take all of our onclick functionality and place it inside the IIFE
+// and then we replace the 'i' in the onlick with the num variable
+
+/*
+function createButtons() {
+    for (var i = 1; i <= 5; i++) {
+        var body = document.getElementsByTagName("BODY")[0];
+        var button = document.createElement("BUTTON");
+        button.innerHTML = 'Button ' + i;
+
+        (function (num) {
+            button.onclick = function () {
+                alert('This is button ' + num);
+            };
+
+        })(i);
+        body.appendChild(button);
+    }
+}
+
+createButtons();
+*/
+// another way to do this
+/*
+function createButtons() {
+    for (var i = 1; i <= 5; i++) {
+        var body = document.getElementsByTagName("BODY")[0];
+        var button = document.createElement("BUTTON");
+        button.innerHTML = 'Button ' + i;
+        addClickFunctionality(button, i);
+        body.appendChild(button);
+    }
+}
+
+createButtons();
+
+function addClickFunctionality(button, num){
+    button.onclick = function () {
+        alert('This is button ' + num);
+    };
+}
+*/
+
+// there is another solution to this problem...almost laughablly simple
+// in the initial loop, use let instead of var - that solves the problem on its own.
+
+function createButtons() {
+    for (let i = 1; i <= 5; i++) {
+      var body = document.getElementsByTagName("BODY")[0];
+      var button = document.createElement("BUTTON");
+      button.innerHTML = 'Button ' + i;
+      button.onclick = function() {
+           alert('This is button ' + i);
+      }
+      body.appendChild(button);
+    }
+ }
+ 
+
+createButtons();
+
+// using let instead of var works here because let is block scoped, not function scope (of which the var keyword is)
+
+//_____________________________________________________________________________________________
+
+// 4th JavaScript question
+
+// Answer the quesiton, "In Javascript, what is a closure?"
+
+// and then code out an example closure that functons properly. 
+
+// again, I have no clue and never heard of the term.
+
+// So what is a closure? oh hell
+
+/*
+
+A Closure is an inner function that has access to the scope of an enclosing function
+
+A closure has access to variables in 3 seperate spaces
+    1. Variables in its own scope
+    2. Variables in the scope of the outer function
+    3. Variables in the global scope
+
+the Closure also has access to 
+    1. its own parameters
+    2. Parameters of outer functions 
+    
+heres an example
+
+
+*/
+
+// the innerFunc in this code is the Closure, but within it, it can still access both the global variable and the outfunctions variable 
+
+const globalVariable = 'global var';
+
+function outterFunc(param1) {
+    const variable1 = 'var one';
+
+    function innerFunc(param2) {
+        const variable2 = 'var two';
+
+        console.log('globalVariable: ', globalVariable);
+        console.log('variable1: ', variable1);
+        console.log('variable2: ', variable2);
+        console.log('param1: ', param1);
+        console.log('param2: ', param2);
+    }
+
+    innerFunc('param one');
+}
+
+outterFunc('param two');
+
+//____________________________________________________________________________________
+
+// 5th Javascript Question 
+
+// What is the 'this' keyword and how is it used?
+
+/*
+
+The 'this' keyword literally refers to whatever object it is inside of.
+
+if you to open the console.log and just console.log(this) it would pull up the global window object
+
+The easiest way to understand this is to use it in contect of an object you have created
+
+heres an example using a house object 
+in the function instead of calling the object     house.price....you can use this instead since this by default refers to the house object
+*/
+
+var house = {
+    price: 100000,
+    squareFeet: 2000,
+    owner: 'Taylor',
+    getPricePerSquareFoot: function() {
+        // return house.price / house.squareFeet;
+           return this.price / this.squareFeet;
+    }
+
+};
+
+// The this keyword will not work outside of the object, like in the console log. 
+
+console.log(house.price);
+//console.log(this.owner);
+console.log(house.getPricePerSquareFoot());
