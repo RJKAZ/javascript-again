d832228c4d54e9cd50c00a0fd0590dbb52b1d7b9
Richard Kazanowski <rjkaz@comcast.net>
2021-03-23 15:34:03 -0400

    added HackerRank folder

---
+++ hackerRankNotes/problemSolvingScript.js
@@ -0,0 +1,22 @@
+// The 1st in the Problem Solving 
+
+// SOLVE ME FIRST!!!
+
+//complete the function solveMeFirst to computer the sum of the two integers, a = 7, b = 3, return 10
+
+function solveMeFirst(a, b){
+console.log( a + b);
+}
+
+solveMeFirst(7, 3);
+
+// ______________________________________________________________________________________________
+
+
+// SIMPLE ARRAY SUM
+
+// Given an Array of integers, find the sum of its elements
+
+function simpleArraySum(ar) {
+  
+}
