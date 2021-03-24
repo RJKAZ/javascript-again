54f77370411659853a24c529021d18a88681b37c
Richard Kazanowski <rjkaz@comcast.net>
2021-03-24 00:20:22 -0400

    Update

--- udemy-complete-js-course/practice.js
+++ udemy-complete-js-course/practice.js
@@ -33,4 +33,67 @@ if (marksBmi > johnsBmi) {
 // now I'm gonna jump to part 2's coding challange #3 since that continues the whole mark/john BMI thing.
 
 // lets use objects to implement the calculations 
+// create a 'calcBMI' method on each object to calculate the BMI
+// store the BMI value to a property and also return it from the method. 
+// log to the console who has a higher BMI together with the full name and the respective BMI. 
 
+let markMiller = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+  
+};
+
+let johnSmith = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+
+  calcBMI: function () {
+    this.bmi = this.mass / this.height **2;
+    return this.bmi;
+  },
+};
+
+markMiller.calcBMI();
+johnSmith.calcBMI();
+
+console.log(johnSmith.bmi, markMiller.bmi);
+
+if(markMiller.bmi > johnSmith.bmi) {
+  console.log(`${markMiller.fullName}'s BMI (${
+    markMiller.bmi}) is higher than ${johnSmith.fullName}'s BMI (${johnSmith.bmi})`)
+} else if (johnSmith.bmi > markMiller.bmi) {
+  console.log(`${johnSmith.fullName}'s BMI (${johnSmith.bmi}) is higher than ${markMiller.fullName}'s BMI (${mark.miller.bmi})`)
+}
+
+// ________________________________________________________________________________________________________
+
+
+// Coding Challenge #3 - Two Gymnastics teams, the Dolphons and Koalas compete three times and the winner with the highest average gets a trophy
+
+// 1. calcuate the average score for each team, using the test data below
+// 2. compare the teams average scores to determine the winner of the competeition and print it to the console. dont' forget there can be a draw. 
+
+// Data 1-  Dolphines socre 96, 108, 89, and Koalas score 88, 91, 110
+
+let dolphinesAverage = (96 + 108 + 89) / 3;
+console.log(dolphinesAverage);
+
+let koalasAverage = (81 + 91 + 100) / 3;
+console.log(dolphinesAverage);
+
+if (dolphinesAverage >= koalasAverage) {
+  console.log("The Dolphines have won!")
+} else if (dolphinesAverage === koalasAverage) {
+  console.log("The teams tied!");
+} else {
+  console.log("Koalas win!");
+}
+
+// Bonus question - include a requirment for a minimum score of 100. with this rule, a team only wins if it has a higher score then the other team. and the same time a score of at least 100 points (use a logical operator to test for minimum scores as well as multiple else-if blocks)
