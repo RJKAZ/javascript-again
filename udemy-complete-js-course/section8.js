a3d4789d55a5ac042bd6f4fd2d1ad24e45a8a918
Richard Kazanowski <rjkaz@comcast.net>
2021-03-31 16:24:48 -0400

    left off on prototype based

---
+++ udemy-complete-js-course/section8.js
@@ -0,0 +1,55 @@
+/*
+
+Lecture Notes - 
+
+JavaScript is a high-level, object-reiented, multi-paradigm progaming language....but thats just the surface.
+
+another definition
+
+JavaScript is a High-Level, prototype-based object-oriented, multi-paradigm, interpreted or just-in-time compiled, dynamic, single-threaded, garbage-collected programming language with first-class functions and a non-blocking event loop concurrency model.  
+
+So lets deconstructe these terms
+
+1. High-Level - 
+
+any computer program needs resources. A Low-level language , the developer has to manage resources (ram, cpu, power, etc) all manually. 
+
+High-Level languages like JavaScript and Phyton, you do not have to manage resources, everything happens automatically thru abstractions that take that work away from the developer. 
+
+This makes the programs easier to use and learn, with the one downside is that high level will never be as fast or optomised as a low-level.
+
+------------------------------------------------------------
+
+2. Garbage-Collected
+
+One of the powerful tools that takes memory management away from the developer is garbage collection. Its a algorithim inside of Javascript that removes or "cleans" the memory so we don't have to. A benefit of a high-level language
+
+-------------------------------------------------------------
+
+3. Interpreted or Just-in-Time compiled
+
+By default, every computer processor only reads in binary, all zeros and ones. Every program has to be written in zeros and ones. (also known as Machine Code). JavaScript as we Humans read and write it, is an abstraction over machine code. But it ultiamtly needs to be converted to machine code. That is called compiling, or interpurting. This is required of all programming languages since no one writes machine code manually.
+
+In Javascript, this compiling/interpurting happens inside the Javascript engine by default
+
+--------------------------------------------------------------------
+
+4. Multi-Paradigm 
+
+A Paradigm is an approach and mindset of strcuturing code, which will direct your coding style and technique. 
+
+Most paradigms are Imperative Vs Declarative
+
+There are three main types of programming 
+  1. Procedural Programming - (organizing code in a very linear way with some functions in between, bascilly we have been doing in this course thus far)
+  2. Object-Oriented Programming (OOP) 
+  3. Functional Programming (FP)
+
+Many Programing languages are only Procedural or only Object orietned, but Javascript does all of it, making it very flexiable and versititle, allowing us to do whatever we want with it. 
+
+-------------------------------------------------------------------------
+
+Prototype-Based Object-Oriented
+
+
+*/
