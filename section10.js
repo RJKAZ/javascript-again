54f8bc6520d72a85a9ec9fb624abba09f86d5671
rjkaz <rjkaz@comcast.net>
2021-05-25 01:22:26 -0400

    closures

--- section10.js
+++ section10.js
@@ -295,3 +295,133 @@ const addTaxRate = function (rate) {
 const addVAT2 = addTaxRate(0.23);
 console.log(addVAT(100));
 console.log(addVAT(23));
+
+
+// Imediantly Invoked Function Expressions ... The IIFE
+
+/*
+Sometime we need a function that is only used once and can then dissapear 
+*/
+
+const runOnce = function () {
+  console.log('This will never run again');
+};
+
+runOnce();
+
+// Now even if I don't intend to run this function ever again, I technically could if I wanted to
+// Like there is nothing stopping us from running the code again
+
+// So what we want to do is instead run a function imediantly without saving it anywhere, kind of like the function self destructs after use so you can only use it the one time
+
+// a workaround is to basicly create a function without a name, which normally Javascript will not let you do
+// so to trick the Javascript into running it, you wrap the whole thing in paraneteses and Javascript will then change from expecting a Statement to instead expecting an expression 
+// and then your add the () at the end to instantly call it. 
+
+(function() {
+  console.log('Now this truely wont ever run again');
+})();
+
+// so this function will now run as a function as an expression, and since it has no name, you can't technically call it again
+
+// This is known as the IIFE - Imediantly Invoked Function Expression
+
+// An even faster way of doing it is with an Arrow Function 
+
+(() => console.log('This will also never run again'))();
+
+// Now why do we have this in Javascript, its important to understand that Functions create scopes
+// One scope does not have access to variables from an inner scope 
+// Where as innerscopes have access to the global scope, but global scope does not have access to the inner scopes. Its purely a one way street. 
+
+// this is known as Data Encapsulation 
+
+// Just or reiterate, Block scoping was introduced with Let and Const. Variables declated with Var will work because Var will ignore the block its scoped in
+// For purposes of security, IIFE aren't really used to so much, but if you need a function called only once, they still have their purpose. 
+
+// NOW FOR CLOSURES - ONE OF THE HARDEST JAVASCRIPT FUNCTIONS TO UNDERSTAND
+// Closures aren't commonly used so its not likely you'll delibretly create one like an Array or a new Function 
+// In certain situations, Closures kind of happen automatically. 
+// So lets create the situation that enabled a closure
+// Below is a function that creates another function that increases the passenger count
+/*
+So, to explain this below function in better detail 
+
+So in the Call Stack (the order in which functions were called)
+So beofre we call secureBooking, it is in the global execution context 
+and in there, the Global EC, we only have that secureBooking function, so that is all that is in the Global Scope
+Then once we call secureBooking, a new execution context is pushed on top of the execution stack
+Now each execution context has a variable envirorment where it contains all its local variables.
+This new execution context contains the passengerCount which is set to 0
+So passengerCount is now in the local scope, and this scope can access all variables of the parent scope, which in this case is just the global scope
+And then a new function is called and stored in the booker variable, so the global context not contains the booker variable
+So when the secureBooking function returns, its execution context pops off the callstack and dissapears 
+This all stuff we have gone over, just going over the callstack and scope chain 
+*/
+
+
+const secureBooking = function() {
+  let passengerCount = 0;
+
+  return function() {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+
+  };
+}
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+// So having called the function 3 times, it increments it each call, from 1 to 2 to 3 passengers
+// But how can this be? Since the secureBooking function has already finished, so its execution context should be gone as well
+// What makes this possibale is a closure. Techncially calling the function 3 times should always return 1 passenger, but somehow it continues to have access to the passenegerCount varibale that should also be gone
+
+// So in a nutshell, a Closure makes a function remember all the variables that existed at the functions birthplace
+// So secureBooking is the birthplace of the function 
+
+/*
+So after booker() is called the first time and finishes executing, its execution context leaves the call stack
+When called again, another execution context is placed on top of the call stack and its empty since it has no variables declared in the function 
+
+The secret of the closure is that any function always has access to the variable enviroment of the execution context in which the function was created. 
+So the Booker variable was born in the execution context of the secureBooking function, so it can still access the passengerCount variable  
+
+to reiterate since this is kind of confusing
+
+1. A function has access to the variable enviroment of the execution context in which it was created
+2. The Closure is that variable enviroment (VE) attached to the function, exactly as it was at the time and place the function was created. 
+
+so the passengerScope variable will always exist in the scope in which the booker function was created.
+
+Closures essentially preserve the scope chain, even when the scope is destroyed at the loss of its execution context
+
+And the function will bascily carry it forever
+
+Thanks to the closure, the function does not lose access to variables that existed at the functions birthplace. 
+
+Here are some formal defintions of closures
+
+Def 1 - A closure is the closed-over variable environment of the execution context in which a function was created, even after that execution context is gone.
+
+Def 2 - A closure gives a function access to all the variables of its parent function, even after that parent function has returned. The function keeps a reference to its outer scope, which preserves the scope chain throughtout time. 
+
+Def 3 - A closure makes sure that a function doesn't loose connection to variables that existed at the functions birth place;
+
+Def 4 - A closure is like a backpack that a function carries wherever it goes. This backpack has all the variables that were present in the enviroment where the function was created. 
+
+IMPORTENT TO REMEMBER - We do NOT Have to manually create closures, this a JavaScript feature that happens automatically. 
+We can't even access closed over variables explicitly. A closure is NOT a tangible JavaScript object.
+
+So while we can't acctually access these variables directly, we can look with console.
+
+*/
+
+console.dir(booker);
+
+
+
+
